03b9bbbf3a35c8cdd74a146492e4042e1e030b84
iliasselkrichi-source <iliass.el.krichi@gmail.com>
2026-04-18 15:02:53 +0200

    Delete supabase/migrations directory

--- supabase/migrations/003_enable_realtime.sql
+++
@@ -1,7 +0,0 @@
--- =====================================================
--- 003_enable_realtime.sql
--- Realtime publicatie inschakelen voor de tabel 'boekingen'
--- =====================================================
-
-ALTER TABLE boekingen REPLICA IDENTITY FULL;
-INSERT INTO supabase_realtime.messages (table_name) VALUES ('boekingen') ON CONFLICT DO NOTHING;
